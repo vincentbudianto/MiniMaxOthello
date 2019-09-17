c320d9de691fc71b72c1e26bb8b181d4d95fe58a
vincent VB <vincent.budianto@yahoo.co.id>
2019-09-17 11:40:44 +0700

    Added Menu
    1. Player vs Minimax moves
    2. Player vs Random moves
    3. Random moves vs Minimax moves

--- MaxOthello.py
+++ MaxOthello.py
@@ -1,4 +1,6 @@
 import pygame
+import os
+import time
 from GameState import GameState
 from Player import RandomPlayer
 from Engine import Engine
@@ -29,8 +31,25 @@ def init(screen, gamestate):
 
 
 def main():
+    validChoice = False
+
+    while not(validChoice):
+        print(" Menu:")
+        print("   1. Player vs Minimax Engine")
+        print("   2. Player vs Random Engine")
+        print("   3. Random Engine vs Minimax Engine")
+        choice = int(input(" >> "))
+        
+        if (choice >= 1) and (choice <= 3):
+            validChoice = True
+        else:
+            input(" Harap masukan pilihan yang valid (1 | 2 | 3)")
+            os.system('cls')    #clear screen for windows
+            #os.system('clear')  #clear screen for linux
+
     screen = pygame.display.set_mode((242, 242))
     gamestate = GameState()
+    randPlayer = RandomPlayer()
     engine = Engine
     init(screen, gamestate)
     running = True
@@ -40,29 +59,78 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                # player put piece
-                mouse_pos = pygame.mouse.get_pos()
-                row = (mouse_pos[1] // 30)
-                col = (mouse_pos[0] // 30)
-                if [row, col] in gamestate.moves[gamestate.turn]:
-                    gamestate.putpiece(row, col)
-                    gamestate.turn = 1 - gamestate.turn
-                    drawboard(screen, gamestate)
-                if not gamestate.moves[gamestate.turn]:
-                    #running = False
-                    break
+                # (White) Player vs Minimax (Black)
+                if choice == 1:
+                    # player put piece
+                    mouse_pos = pygame.mouse.get_pos()
+                    row = (mouse_pos[1] // 30)
+                    col = (mouse_pos[0] // 30)
+                    if [row, col] in gamestate.moves[gamestate.turn]:
+                        gamestate.putpiece(row, col)
+                        gamestate.turn = 1 - gamestate.turn
+                        drawboard(screen, gamestate)
+                    if not gamestate.moves[gamestate.turn]:
+                        #running = False
+                        break
 
-                # engine put piece
-                new_state = GameState(gamestate)
-                row, col = engine.get_move(engine, new_state, new_state.turn)
-                if [row, col] in gamestate.moves[gamestate.turn]:
-                    gamestate.putpiece(row, col)
-                    gamestate.turn = 1 - gamestate.turn
-                    drawboard(screen, gamestate)
-                if not gamestate.moves[gamestate.turn]:
-                    #running = False
-                    break
+                    # minimax engine put piece
+                    new_state = GameState(gamestate)
+                    row, col = engine.get_move(engine, new_state, new_state.turn)
+                    if [row, col] in gamestate.moves[gamestate.turn]:
+                        gamestate.putpiece(row, col)
+                        gamestate.turn = 1 - gamestate.turn
+                        drawboard(screen, gamestate)
+                    if not gamestate.moves[gamestate.turn]:
+                        #running = False
+                        break
+                # (White) Player vs Random (Black)
+                elif choice == 2:
+                    # player put piece
+                    mouse_pos = pygame.mouse.get_pos()
+                    row = (mouse_pos[1] // 30)
+                    col = (mouse_pos[0] // 30)
+                    if [row, col] in gamestate.moves[gamestate.turn]:
+                        gamestate.putpiece(row, col)
+                        gamestate.turn = 1 - gamestate.turn
+                        drawboard(screen, gamestate)
+                    if not gamestate.moves[gamestate.turn]:
+                        #running = False
+                        break
 
+                    # random engine put piece
+                    new_state = GameState(gamestate)
+                    row, col = randPlayer.move(new_state)
+                    if [row, col] in gamestate.moves[gamestate.turn]:
+                        gamestate.putpiece(row, col)
+                        gamestate.turn = 1 - gamestate.turn
+                        drawboard(screen, gamestate)
+                    if not gamestate.moves[gamestate.turn]:
+                        #running = False
+                        break
+                # (White) Random vs Minimax (Black)
+                elif choice == 3:
+                    # random engine put piece
+                    new_state = GameState(gamestate)
+                    row, col = randPlayer.move(new_state)
+                    if [row, col] in gamestate.moves[gamestate.turn]:
+                        gamestate.putpiece(row, col)
+                        gamestate.turn = 1 - gamestate.turn
+                        drawboard(screen, gamestate)
+                    if not gamestate.moves[gamestate.turn]:
+                        #running = False
+                        break
+                    
+                    # minimax engine put piece
+                    new_state = GameState(gamestate)
+                    row, col = engine.get_move(engine, new_state, new_state.turn)
+                    if [row, col] in gamestate.moves[gamestate.turn]:
+                        gamestate.putpiece(row, col)
+                        gamestate.turn = 1 - gamestate.turn
+                        drawboard(screen, gamestate)
+                    if not gamestate.moves[gamestate.turn]:
+                        #running = False
+                        break
+                    
                 print("Current score: " + str(gamestate.count(True)) + "  " + str(gamestate.count(False)))
 
     p1_count = str(gamestate.count(True))
@@ -74,8 +142,6 @@ def main():
       print("CPU WIN")
     else:
       print("DRAW")
-
-    input()
 
     running = False
 
